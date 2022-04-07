1e7763785934c27a733b98787349eb9332b0a6c1
satoru.kanaya <satoru.kanaya@fastretailing.com>
2022-04-07 11:42:54 +0000

    Lesson8_no_kadai

---
+++ mentor.rb
@@ -0,0 +1,26 @@
+class Mentor
+
+  def initialize(name)
+   @name = name
+  end
+  
+  def job()
+   p "#{@name}です。私は現役のITプロフェッショナルです。"
+  end
+end
+
+class Railsmentor < Mentor
+
+  def initialize(name)
+   @name = name
+  end
+  
+  def job()
+   p "#{@name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+  end
+end
+
+mentor1=Mentor.new("煌木")
+mentor1.job
+railsmentor1=Railsmentor.new("赤出")
+railsmentor1.job
